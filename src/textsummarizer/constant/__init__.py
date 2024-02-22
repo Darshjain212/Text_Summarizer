6bb7e83919b11b1e1b0f1cb9532c98219991200d
unknown <darshjain368@gmail.com>
2024-02-22 17:54:23 +0530

    data_ingestion notebook added

---
+++ src/textsummarizer/constant/__init__.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+# CONFIG_FILE_PATH = "config/config.yaml"
+# PARAMS_FILE_PATH = "params.yaml"
+
+path_to_yaml = "config/config.yaml"
+CONFIG_FILE_PATH = Path(path_to_yaml)
+
+path_to_param = "params.yaml"
+PARAMS_FILE_PATH = Path(path_to_yaml)
+
